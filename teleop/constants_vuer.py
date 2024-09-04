fdc8dd44f315bd3aeb5ccd5c329a25ffcebaeb01
silencht <silencht@qq.com>
2024-09-04 17:25:54 +0800

    [fix] fix constants matrixs bug.

--- teleop/constants_vuer.py
+++ teleop/constants_vuer.py
@@ -4,12 +4,12 @@ tip_indices = [4, 9, 14, 19, 24]
 
 hand2inspire_l_arm = np.array([[1, 0, 0, 0],
                                [0, 0, -1, 0],
-                               [1, 1, 0, 0],
+                               [0, 1, 0, 0],
                                [0, 0, 0, 1]])
 
 hand2inspire_r_arm = np.array([[1, 0, 0, 0],
                                [0, 0, 1, 0],
-                               [1, -1, 0, 0],
+                               [0, -1, 0, 0],
                                [0, 0, 0, 1]])
 
 hand2inspire_l_finger = np.array([[0, -1, 0, 0],
